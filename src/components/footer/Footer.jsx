bd098565f422d614fa7e1f45e23495701535cc97
Eyvlina <evylina5@gmail.com>
2024-03-20 20:46:38 +0000

    toast added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,97 +1,3 @@
-// import "./footer.css";
-// import { useRef } from "react";
-// import emailjs from "@emailjs/browser";
-// import { MdOutlineMail } from "react-icons/md";
-// import { CiLinkedin } from "react-icons/ci";
-// import { IoLogoGithub } from "react-icons/io";
-// import { IoLogoInstagram } from "react-icons/io";
-// import Divider from "@mui/material/Divider";
-// import { BsFillSendFill } from "react-icons/bs";
-// import { GrContact } from "react-icons/gr";
-
-// export default function Footer({ footerRef }) {
-//   const form = useRef();
-
-//   const sendEmail = (e) => {
-//     e.preventDefault();
-
-//     emailjs
-//       .sendForm("service_5m0ah4g", "template_srobx3j", form.current, {
-//         publicKey: "KDxlCFiHYcoL1x2XF",
-//       })
-//       .then(
-//         () => {
-//           console.log("SUCCESS!");
-//           form.current.reset();
-//         },
-//         (error) => {
-//           console.log("FAILED...", error.text);
-//         }
-//       );
-//   };
-//   return (
-
-//     <section id="footerSection" ref={footerRef}>
-//       <footer>
-//         <div className="footerContent">
-//           <div className="social">
-//             {/* <div className="footer_text"> */}
-//               <h3 id="footer_heading">Get In Touch&#xa0;<GrContact className="msgIcon"/></h3>
-//               <p>
-//                 Want to get to know me better ? Let's grab a coffee(in-person or
-//                 virtually)! You can also find me on socail media or just drop a
-//                 message below.
-//               </p>
-//               <ul className="footer_ul">
-//                 <li>
-//                   <MdOutlineMail className="logos" />
-//                 </li>
-//                 <li>
-//                   <CiLinkedin className="logos" />
-//                 </li>
-//                 <li>
-//                   <IoLogoGithub className="logos" />
-//                 </li>
-//                 <li>
-//                   <IoLogoInstagram className="logos" />
-//                 </li>
-//               </ul>
-//             {/* </div> */}
-//           </div>
-//           <Divider flexItem className="smallHR">Or</Divider>
-//           <div className="emailJSForm">
-//             <form ref={form} onSubmit={sendEmail}>
-//               <label className="eb">Your Name</label>
-//               <input
-//                 type="text"
-//                 name="user_name"
-//                 className="input_box"
-//                 required
-//               />
-//               <label>Your Email</label>
-//               <input
-//                 type="email"
-//                 name="user_email"
-//                 className="input_box"
-//                 required
-//               />
-//               <label>Message</label>
-//               <textarea name="message" className="input_box" required />
-//               <button
-//                 type="submit"
-//                 className="btn btn-outline btn-primary btn-xs mt-3"
-//               >
-//                 <BsFillSendFill />
-//               </button>
-//             </form>
-//           </div>
-//         </div>
-//         <div className="footerRights">Coded with &#10084; by Evylina</div>
-//       </footer>
-//     </section>
-//   );
-// }
-
 import "./footer.css";
 import { useRef } from "react";
 import emailjs from "@emailjs/browser";
@@ -102,9 +8,19 @@ import { IoLogoInstagram } from "react-icons/io";
 import Divider from "@mui/material/Divider";
 import { BsFillSendFill } from "react-icons/bs";
 import { GrContact } from "react-icons/gr";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function Footer({ footerRef }) {
   const form = useRef();
+
+  const notify = () => toast.success('Message sent successfully.', {
+    style: {
+      background: 'rgb(0, 170, 110)', 
+      padding: '1em',
+      color: 'white',
+      fontFamily: "ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Vazirmatn, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji"
+    },
+  });
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -117,6 +33,7 @@ export default function Footer({ footerRef }) {
         () => {
           console.log("SUCCESS!");
           form.current.reset();
+          notify()
         },
         (error) => {
           console.log("FAILED...", error.text);
@@ -195,6 +112,9 @@ export default function Footer({ footerRef }) {
               >
                 <BsFillSendFill />
               </button>
+              <Toaster
+                style={{ background: "green !important", color: "white" }}
+              />
             </form>
           </div>
         </div>
